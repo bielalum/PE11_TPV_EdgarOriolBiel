61e729e84166d91fc3f91e81f220825c1e7f19a1
bielalum <bielalum@gmail.com>
2026-05-14 19:01:36 +0200

    S'han corregit errors, actualitzant el codi

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,11 +8,11 @@ public class Main {
     public static void main(String[] args) {
         Connexio.connectar();
 
-        Camisa novaCamisa = new Camisa(1, "Camisa de Botons Blanca", 25.5, 10, 42);
-        Pantalo nouPantalo = new Pantalo(2, "Texans Blaus", 40.0, 5, 38);
+        Camisa novaCamisa = new Camisa(1, "Camisa de Botons Blanca", 25.5, 21, 10, 42, 12);
+        Pantalo nouPantalo = new Pantalo(2, "Texans Blaus", 40.0, 21, 5, 38, 40);
 
         System.out.println("Objectes creats:");
-        System.out.println("Camisa: " + novaCamisa.getNom() + " (Talla: " + novaCamisa.getTallaColl() + ")");
-        System.out.println("Pantalo: " + nouPantalo.getNom() + " (Amplada: " + nouPantalo.getAmpladaPit() + ")");
+        System.out.println("Camisa: " + novaCamisa.getNom() + " (Talla coll: " + novaCamisa.getTallaColl() + ", Talla pit: " + novaCamisa.getAmpladaPit() + ")");
+        System.out.println("Pantalo: " + nouPantalo.getNom() + " (Talla cintura: " + nouPantalo.getTallaCintura() + ", Talla camal: " + nouPantalo.getLlargadaCamal() + ")");
     }
 }
